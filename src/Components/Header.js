8e7dfe7da2f66d3ae910adf731be5902b17d681f
Elberreth <unix555@gmail.com>
2024-05-10 14:58:51 +0200

    First prototype

---
+++ src/Components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../CSS/Header.css'; 
+import logo from '../Images/Jambiz.png'; 
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" /> 
+      </div>
+      <div className="links-container">
+        <Link to="/login" className="link">Login</Link>
+        <Link to="/register" className="link">Register</Link>
+        
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
