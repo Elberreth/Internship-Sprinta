da5ede95ef26a799724cff16ada8131ae606fbdb
Elberreth <unix555@gmail.com>
2024-05-14 23:58:52 +0200

    Start page, and Register page WIP

--- src/Pages/AgreementPopup.js
+++ src/Pages/AgreementPopup.js
@@ -6,7 +6,9 @@ const AgreementPopup = ({ onClose }) => {
     <div className="popup">
       <div className="popup-content">
         <h3>Agreement Policy</h3>
-        <p>Här kommer innehållet i din agreement policy...</p>
+        <p>A User Agreement is an excellent addition to a website or mobile app for a number of reasons. It helps you set out the rules and requirements for users who visit your site or download your app.
+
+This article will help you understand if a User Agreement is something you want, and if so, how to write and display your own.</p>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
