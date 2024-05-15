a4ee4096c19d1d78cf1afc827570ab5a717d62e5
Elberreth <unix555@gmail.com>
2024-05-15 18:06:58 +0200

    Changes to user agreement

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,7 +4,7 @@ import '../CSS/About.css'; // Importera CSS-filen för styling
 const About = () => {
   return (
     <div className="about-container">
-      <h2>About Us</h2>
+      <h2>About this site</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique. Massa id neque aliquam vestibulum.
       </p>
