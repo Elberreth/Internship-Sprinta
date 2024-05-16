659443b869eb9bb4d4c5f00062b326420b206a8d
Elberreth <unix555@gmail.com>
2024-05-16 15:07:06 +0200

    Merged and updated

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,9 +4,9 @@ import '../CSS/About.css'; // Importera CSS-filen för styling
 const About = () => {
   return (
     <div className="about-container">
-      <h2>About this site</h2>
+      <h2><b>About this site</b></h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique. Massa id neque aliquam vestibulum.
+       <h4> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique. Massa id neque aliquam vestibulum.</h4>
       </p>
     </div>
   );
