3b65cfe1e2083f13a85d4c50a005594b1a8aa1f2
Elberreth <unix555@gmail.com>
2024-05-29 14:52:29 +0200

    Added popup to AdminPage

--- src/Pages/NewlyRegisteredUsers.js
+++ src/Pages/NewlyRegisteredUsers.js
@@ -1,58 +1,40 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button } from 'react-bootstrap';
 
 const NewlyRegisteredUsers = () => {
   const [newUsers, setNewUsers] = useState([
-    { name: 'Kurt Kurtson', email: 'kurt@xbus.com', company: 'XBUS', employed: true },
-    { name: 'Conny Connysson', email: 'conny@example.com', company: 'EXCEED', employed: false },
-    { name: 'Daniel Carlsson', email: 'unix555@gmail.com', company: 'Sprinta', employed: false }
+    { id: 1, name: 'Kurt Kurtson', email: 'kurt@xbus.com', company: 'XBUS', employed: true },
+    { id: 2, name: 'Conny Connysson', email: 'conny@example.com', company: 'EXCEED', employed: false },
+    { id: 3, name: 'Daniel Carlsson', email: 'unix555@gmail.com', company: 'Sprinta', employed: false }
   ]);
   const [checkedUsers, setCheckedUsers] = useState([]);
-  const [acceptedUsers, setAcceptedUsers] = useState([]);
-  const [rejectedUsers, setRejectedUsers] = useState([]);
-  const [showAccepted, setShowAccepted] = useState(false);
-  const [showRejected, setShowRejected] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalAction, setModalAction] = useState(null);
 
-  const handleCheckboxChange = (user) => {
-    if (checkedUsers.includes(user)) {
-      setCheckedUsers(checkedUsers.filter(u => u !== user));
+  const handleCheckboxChange = (userId) => {
+    if (checkedUsers.includes(userId)) {
+      setCheckedUsers(checkedUsers.filter(id => id !== userId));
     } else {
-      setCheckedUsers([...checkedUsers, user]);
+      setCheckedUsers([...checkedUsers, userId]);
     }
   };
 
-  const handleAccept = () => {
-    setAcceptedUsers([...acceptedUsers, ...checkedUsers]);
+  const handleAction = (action) => {
+    setModalAction(action);
+    setShowModal(true);
+  };
+
+  const handleConfirmAction = () => {
+    if (modalAction === 'accept') {
+      const newlyAcceptedUsers = checkedUsers;
+      setNewUsers(newUsers.filter(user => !newlyAcceptedUsers.includes(user.id)));
+    } else if (modalAction === 'reject') {
+      const newlyRejectedUsers = checkedUsers;
+      setNewUsers(newUsers.filter(user => !newlyRejectedUsers.includes(user.id)));
+    }
     setCheckedUsers([]);
-    setShowAccepted(true);
-  };
-
-  const handleReject = () => {
-    setRejectedUsers([...rejectedUsers, ...checkedUsers]);
-    setCheckedUsers([]);
-    setShowRejected(true);
-  };
-
-  const handleCancelAccept = () => {
-    setShowAccepted(false);
-    setAcceptedUsers([]);
-  };
-
-  const handleCancelReject = () => {
-    setShowRejected(false);
-    setRejectedUsers([]);
-  };
-
-  const finalizeAccept = () => {
-    setNewUsers(newUsers.filter(user => !acceptedUsers.includes(user)));
-    setAcceptedUsers([]);
-    setShowAccepted(false);
-  };
-
-  const finalizeReject = () => {
-    setNewUsers(newUsers.filter(user => !rejectedUsers.includes(user)));
-    setRejectedUsers([]);
-    setShowRejected(false);
+    setShowModal(false);
   };
 
   return (
@@ -70,7 +52,7 @@ const NewlyRegisteredUsers = () => {
       {newUsers.map((user, index) => (
         <div key={index} className="row border-top p-3">
           <div className="col">
-            <input type="checkbox" onChange={() => handleCheckboxChange(user)} checked={checkedUsers.includes(user)} />
+            <input type="checkbox" onChange={() => handleCheckboxChange(user.id)} checked={checkedUsers.includes(user.id)} />
           </div>
           <div className="col">{user.name.split(' ')[1]}</div>
           <div className="col">{user.name.split(' ')[0]}</div>
@@ -82,49 +64,52 @@ const NewlyRegisteredUsers = () => {
       {/* Accept and Reject buttons */}
       <div className="row mt-3" style={{ maxWidth: '200px', margin: 'auto' }}>
         <div className="col d-flex justify-content-center">
-          <button type="button" className="btn btn-primary btn-sm mr-2" style={{ fontSize: '10px', marginRight: '10px' }} onClick={handleAccept}>Accept</button>
-          <button type="button" className="btn btn-danger btn-sm" style={{ fontSize: '10px', marginLeft: '10px' }} onClick={handleReject}>Reject</button>
+          <Button
+            variant="primary"
+            className="btn-sm-custom mr-2"
+            onClick={() => handleAction('accept')}
+            disabled={checkedUsers.length === 0}
+          >
+            Accept
+          </Button>
+          <Button
+            variant="danger"
+            className="btn-sm-custom"
+            onClick={() => handleAction('reject')}
+            disabled={checkedUsers.length === 0}
+          >
+            Reject
+          </Button>
         </div>
       </div>
-      {/* Accepted Users */}
-      {showAccepted && acceptedUsers.length > 0 && (
-        <div className="mt-3">
-          <h5>Accepted Users</h5>
-          <ul className="list-group">
-            {acceptedUsers.map((user, index) => (
-              <li key={index} className="list-group-item">
-                {user.name} - {user.email}
-              </li>
-            ))}
-          </ul>
-          <div className="mt-2 d-flex justify-content-center">
-            <button className="btn btn-primary btn-sm mr-2" style={{ width: '80px', fontSize: '10px', marginRight: '10px' }} onClick={finalizeAccept}>Accept</button>
-            <button className="btn btn-secondary btn-sm" style={{ width: '80px', fontSize: '10px', marginLeft: '10px' }} onClick={handleCancelAccept}>Cancel</button>
-          </div>
-        </div>
-      )}
-      {/* Rejected Users */}
-      {showRejected && rejectedUsers.length > 0 && (
-        <div className="mt-3">
-          <h5>Rejected Users</h5>
-          <ul className="list-group">
-            {rejectedUsers.map((user, index) => (
-              <li key={index} className="list-group-item">
-                {user.name} - {user.email}
-              </li>
-            ))}
-          </ul>
-          <div className="mt-2 d-flex justify-content-center">
-            <button className="btn btn-primary btn-sm mr-2" style={{ width: '80px', fontSize: '10px', marginRight: '10px' }} onClick={finalizeReject}>Accept</button>
-            <button className="btn btn-secondary btn-sm" style={{ width: '80px', fontSize: '10px', marginLeft: '10px' }} onClick={handleCancelReject}>Cancel</button>
-          </div>
-        </div>
-      )}
+
+      {/* Modal */}
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Action</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to {modalAction} the selected users?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" className="btn-sm-popup" onClick={handleConfirmAction}>
+            Confirm
+          </Button>
+          <Button variant="secondary" className="btn-sm-popup" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
 
 export default NewlyRegisteredUsers;
+
+
+
+
+
+
+
 
 
 
