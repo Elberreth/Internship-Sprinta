d9b6938015e8bbc4875b2b50a26419cb5e3913e0
Elberreth <unix555@gmail.com>
2024-06-19 22:18:39 +0200

    Bug fix

--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../CSS/AdminPage.css';
 import NewlyRegisteredUsers from './NewlyRegisteredUsers';
 import AllUsers from './AllUsers';
-import Companies from './Companies';
+import Organisations from './Companies'; 
 import Roles from './Roles';
 
 const AdminPage = () => {
@@ -15,7 +15,7 @@ const AdminPage = () => {
     setShowDropdown(false);
   };
 
-  const menuOptions = ['New Users', 'All Users', 'Companies', 'Access Management'];
+  const menuOptions = ['New Users', 'All Users', 'Organisations', 'Access Management']; 
 
   return (
     <div className="admin-page">
@@ -59,10 +59,10 @@ const AdminPage = () => {
             <AllUsers />
           </>
         )}
-        {selectedMenu === 'Companies' && (
+        {selectedMenu === 'Organisations' && ( 
           <>
-            <h3 className="content-title">Companies</h3>
-            <Companies />
+            <h3 className="content-title">Organisations</h3> 
+            <Organisations />
           </>
         )}
         {selectedMenu === 'Access Management' && (
@@ -77,6 +77,7 @@ const AdminPage = () => {
 };
 
 export default AdminPage;
+
 
 
 
