685de67469eedc508ef912fcf5b8d6f8f4b198bd
Anita Ruth Sweetlin <anitaruthsweetlin@gmail.com>
2024-07-01 16:29:59 +0530

    Removed hardcoded values and Reads data using API

--- src/Utils/OrganisationList.js
+++ src/Utils/OrganisationList.js
@@ -1,15 +1,23 @@
-// src/Utils/OrganisationList.js
+import axios from 'axios';
 
-const organisationList = [
-  "XBUS",
-  "EXCEED",
-  "Sprinta",
-  "Progress Lead",
-  "ADDENDS",
-  "PODIUM",
-  "Analytic Lead",
-  "LearningLEAD",
-  "SwCG",
-];
+const organisationList = await getOrganizationNames();
+
+async function getOrganizationNames() {
+  try {
+
+    const response = await axios.get('http://localhost:8080/organisation');
+    const organizationData = response.data.data;
+    if (organizationData && Array.isArray(organizationData.organisationDTOViewList)) {
+      const organizationNames = organizationData.organisationDTOViewList.map(org => org.name);
+      return organizationNames;
+    } else {
+      console.error('Unexpected data format:', organizationData);
+      return ['No Data'];
+    }
+  } catch (error) {
+    console.error('Error fetching organization data:', error);
+    return ['No Data'];
+  }
+}
 
 export default organisationList;
